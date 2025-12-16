3584f15a796c31e6dbaee02674d868c54188fb90
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-16 15:02:53 -0500

    Update comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 let users = [];
 
-// Display Users
+// Display users
 function displayUsers(users) {
+  // DOM element selection
   const galleryDiv = document.querySelector("#gallery");
+  // Iterate over the users array
   users.forEach((user) => {
+    // Card HTML string
     const userHTML = `
       <div class="card" data-email="${user.email}">
         <div class="card-img-container">
@@ -16,22 +19,29 @@ function displayUsers(users) {
         </div>
       </div>
     `;
+    // Insert the HTML into the #gallery div
     galleryDiv.insertAdjacentHTML("beforeend", userHTML);
   });
 }
 
-// Get 12 Random Users
+// Get 12 random users
 async function getUsers() {
+  // URL for the API endpoint
   const url = "https://randomuser.me/api/?results=12&nat=us";
   try {
     const response = await fetch(url);
+    // Check if the request was successful
     if (!response.ok) {
+      // Throw an error with the status message
       throw new Error(`Response status: ${response.status}`);
     }
+    // Parse the response body as JSON
     const data = await response.json();
+    // Set users array and display users
     users = data.results;
     displayUsers(users);
   } catch (error) {
+    // Catch any errors
     console.log(error.message);
   }
 }
