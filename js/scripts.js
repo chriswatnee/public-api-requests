3f1e35695bfa8b389c04887f1c8427159a3dfce6
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-12-17 09:05:28 -0500

    Display modal when employee card is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,50 @@
+// Stores user data
 let users = [];
+
+// DOM element selection
+const galleryDiv = document.querySelector("#gallery");
+
+// Display user modal
+function displayUserModal(user) {
+  const getFormattedDate = (dateStr) => new Date(dateStr).toLocaleDateString("en-US");
+  const userModalHTML = `
+    <div class="modal-container">
+      <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+          <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+          <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="modal-text">${user.email}</p>
+          <p class="modal-text cap">${user.location.city}</p>
+          <hr>
+          <p class="modal-text">${user.cell}</p>
+          <p class="modal-text">${user.location.street.number} ${user.location.street.name},  ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
+          <p class="modal-text">Birthday: ${getFormattedDate(user.dob.date)}</p>
+        </div>
+      </div>
+    </div>
+  `;
+  document.body.insertAdjacentHTML("beforeend", userModalHTML);
+}
+
+// Add a click event listener to the #gallery div
+galleryDiv.addEventListener("click", (e) => {
+  // Find the nearest ancestor element with the class card
+  const userCard = e.target.closest(".card");
+  // If a user card was not clicked then stop code from executing
+  if (!userCard) {
+    return;
+  }
+  // Get the user's email from the card's email data attribute
+  const userEmail = userCard.dataset.email;
+  // Get the user from the email
+  const user = users.find((user) => user.email === userEmail);
+  // Display modal for user
+  displayUserModal(user);
+});
 
 // Display users
 function displayUsers(users) {
-  // DOM element selection
-  const galleryDiv = document.querySelector("#gallery");
   // Iterate over the users array
   users.forEach((user) => {
     // Card HTML string
